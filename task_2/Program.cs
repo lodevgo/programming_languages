a22acb73d482fc8ac02f8b2055cb9a2c9206d399
logodev <pumpgem@gmail.com>
2024-07-17 11:51:57 +0300

    сделали второе задание по работе с различным типом данных, занимались арифметикой также

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿int bas = 100;
+int sab = 500;
+int sum = bas + sab; 
+Console.WriteLine(sum);
+
+long l1 = 6; 
+long sum2 = bas + l1;
+Console.WriteLine(sum2);
+
+double fas = 2.5;
+double sum3 = fas + sum; 
+
+int product1 = bas * sab;
+long product2 = bas * sum;
+double product3 = sum3 * product2;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
