066110d87d957389d9fc260f58c64caf9e2ab1ae
logodev <pumpgem@gmail.com>
2024-07-17 11:17:04 +0300

    работа с типом данных

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello world!");
+﻿// Console.WriteLine("Hello world!");   
+
+using System.Reflection.Metadata;
+
+int numbers = 100;
+Console.WriteLine(numbers);
+
+double d = 100.5555555;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
