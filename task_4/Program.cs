9c3daaee278f29d609a47257ad3be388fd5ba5df
logodev <pumpgem@gmail.com>
2024-07-17 14:48:21 +0300

    сделали 4 задание на сравнение гирь

---
+++ task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 15;
+int b = 115;
+int c = 1234;
+int d = 111;
+int e = 124;
+
+int max = a; 
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
